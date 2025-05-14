ad6b4cc6aeec393a10b4c8dcc611aa6ad4b115ed
MH <gaga7589@gmail.com>
2025-05-14 13:04:15 +0900

    Merge branch 'master' of https://github.com/hun7589/HelloWorld.git

--- HelloWorld/src/com/yedam/JSExe.java
+++ HelloWorld/src/com/yedam/JSExe.java
@@ -2,15 +2,15 @@ package com.yedam;
 
 public class JSExe {
 	public static void main(String[] args) {
-		// 임의의 수를 생성. 1~1000 사이의 임의의 값 생성   .
-		// 1<= x < 11 의 값
-		int sum = 0;
-		for (int i = 1; i <= 5; i++) {
-			int result = (int) (Math.random() * 10) + 1;
-			sum += result;
-		}
-		System.out.println("결과: "+sum);
-		test();
+		int num1 = 30;
+		int num2 = 30;
+		System.out.println(num1==num2);
+		
+		String str1 =new String("Hello");
+		String str2 =new String("Hello");
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str1.equals(str2));//문자열을 비교할 때에는 equals를 사용함.
 	}// end of main.
 
 	public static void test() {
@@ -24,5 +24,14 @@ public class JSExe {
 		System.out.println("1에서 10까지 홀수를 더한 값은 : " + sum);
 
 	}// end of test
-
+	public static void test2() {
+		// 임의의 수를 생성. 1~1000 사이의 임의의 값 생성   .
+				// 1<= x < 11 의 값
+				int sum = 0;
+				for (int i = 1; i <= 5; i++) {
+					int result = (int)(Math.random() * 10) + 1;
+					sum += result;
+				}
+				System.out.println("결과: "+sum);
+	}//end of test2
 }// end of JSExe
